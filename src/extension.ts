974ed42f336468e0d76a45fdc407fde3904bc0cd
Ho-Wan <ho.wan2345@gmail.com>
2018-10-22 00:01:46 +0100

    Trello npm package used to get list.

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,29 @@
-'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+"use strict";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+import * as vscode from "vscode";
+import * as Trello from "trello";
+import * as keys from "./config/keys";
+
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "trello" is now active!');
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "trello" is now active!');
+  let disposable = vscode.commands.registerCommand("trello.sayHello", () => {
+    vscode.window.showInformationMessage(
+      "Hello, this is the Trello extension!"
+    );
+  });
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+  let getCard = vscode.commands.registerCommand("trello.getCard", () => {
+    let trello = new Trello(keys.my_api_key, keys.my_api_token);
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    let cardsPromise = trello.getCardsOnList("5bccf456adf6930b49d0c468");
+    cardsPromise.then(cards => {
+      console.log(cards);
     });
+  });
 
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
+  context.subscriptions.push(getCard);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
