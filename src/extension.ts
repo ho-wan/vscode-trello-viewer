693ddfe980413b4b19bc3934407af85103e93df6
Ho-Wan <ho.wan2345@gmail.com>
2018-12-08 12:25:25 +0000

    added TestView to show TreeView for nodeDependencies

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,24 @@
 import * as vscode from "vscode";
 import { TrelloComponent, removeTempTrelloFile } from "./trelloUtils";
+import { TestView } from "./testView";
 
 const tempTrelloFileName = '~vscodeTrello.md';
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "trello" is now active!');
+  // @ts-ignore
+  const testView = new TestView(vscode.workspace.workspaceFolders[0].uri.fsPath);
+  vscode.window.registerTreeDataProvider('testView', testView);
 
   const trello = new TrelloComponent(context);
-  let test = vscode.commands.registerCommand("trello.test", () => trello.getTrelloKeyToken());
-  let setKey = vscode.commands.registerCommand("trello.setKey", () => trello.setTrelloKey());
-  let setToken = vscode.commands.registerCommand("trello.setToken", () => trello.setTrelloToken());
-  let getBoards = vscode.commands.registerCommand("trello.getBoards", () => trello.getTrelloBoards());
-  let getCard = vscode.commands.registerCommand("trello.getCard", () => trello.getTrelloCards());
-  let showCard = vscode.commands.registerTextEditorCommand('trello.showCard', () => {
-    trello.showTrelloCard(tempTrelloFileName)
-  });
-
-  context.subscriptions.push(test);
-  context.subscriptions.push(setKey);
-  context.subscriptions.push(setToken);
-  context.subscriptions.push(getBoards);
-  context.subscriptions.push(getCard);
-  context.subscriptions.push(showCard);
+  context.subscriptions.push(vscode.commands.registerCommand("trello.test", () => trello.getTrelloKeyToken()));
+  context.subscriptions.push(vscode.commands.registerCommand("trello.setKey", () => trello.setTrelloKey()));
+  context.subscriptions.push(vscode.commands.registerCommand("trello.setToken", () => trello.setTrelloToken()));
+  context.subscriptions.push(vscode.commands.registerCommand("trello.getBoards", () => trello.getTrelloBoards()));
+  context.subscriptions.push(vscode.commands.registerCommand("trello.getCard", () => trello.getTrelloCards()));
+  context.subscriptions.push(
+    vscode.commands.registerTextEditorCommand('trello.showCard', () => {trello.showTrelloCard(tempTrelloFileName)})
+  );
 }
 
 export function deactivate() {
